5f12e3dbc41043e8d1ae6fdb2f1f76bce6e3fc39
hemangikarkar2 <55585140+hemangikarkar2@users.noreply.github.com>
2024-04-25 14:46:09 +1000

    Time: 0 ms (100.00%), Space: 41.2 MB (87.82%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+       int[] year=new int[101];
+        for(int[] log:logs){
+            year[log[0]-1950]++;
+            year[log[1]-1950]--;
+        }
+        int maxNum=year[0],maxYear=1950;
+        for(int i=1;i<year.length;i++){
+            year[i] += year[i-1];
+            if(year[i]>maxNum){
+                maxNum=year[i];
+                maxYear=i+1950;
+            }
+        }
+        return maxYear;
+        
+
+          
+    }
+  
+}
+
+/**Let's understand with an example
+Suppose we are given the log [1950, 1961]
+This means birth year is 1950 and death year is 1961
+Therefore, years from 1950 to 1960 will be incremented by 1.
+
+We can do it by iterating from 1950 to 1960 but that will increase time complexity if we have to do it for every query in logs array.
+
+To do this in O(1), we increment year[1950] by 1 and decrement year[1961] by 1.
+We can reapeat this for every query in logs array.
+
+What will this do ?
+
+For the case [1950, 1961], let's look at how the array will look like
+
+image
+
+But this is not the desired result ?
+
+To get the answer,
+After iterating through all the queries, take prefix sum of the array(year)
+This is how the array will look like
+
+image
+
+You can see that the Prefix Sum row will give the desired result as we have incremented the values of array from index 1950 to 1960.
+
+Let's try for the test case, logs = [[1950,1961],[1960,1965],[1963,1970]] for a better understanding
+
+image
+
+Looking at the Prefix Sum, we can clearly see that the maximum value is 2 and its first occurence is at 1960. Hence, 1960 is the answer.
+**/
